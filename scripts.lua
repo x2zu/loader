8057d38f1e2353255805e700be8694940b07de32
x2zu <nooelprakasa@gmail.com>
2025-06-10 21:32:45 +0700

    Update scripts.lua

--- scripts.lua
+++ scripts.lua
@@ -1,20 +1,26 @@
-if not game:IsLoaded() then
-    game.Loaded:Wait()
-end
+game.Loaded:Wait()
 
 local MarketplaceService = game:GetService("MarketplaceService")
 local creator = MarketplaceService:GetProductInfo(game.PlaceId).Creator.CreatorTargetId
 
 local script_key = false
 
-if creator == 2753915549 then
-    local url
-    if script_key then
-        url = "https://raw.githubusercontent.com/x2zu/loader/refs/heads/main/ObfSource/BloxFruits.lua" -- Blox Fruits
+local supportedGames = {
+    [101949297449238] = "https://raw.githubusercontent.com/x2zu/loader/refs/heads/main/ObfSource/Build%20An%20Island.lua", -- Build Island
+    [18172550962] = "https://raw.githubusercontent.com/x2zu/loader/refs/heads/main/ObfSource/Pixel%20Blade.lua", -- Pixel Blade
+    [94682676231618] = "https://raw.githubusercontent.com/x2zu/loader/refs/heads/main/ObfSource/My%20Fishing%20Pier.lua", -- My Fishing Pier
+    [126884695634066] = "https://raw.githubusercontent.com/x2zu/loader/refs/heads/main/ObfSource/BloxFruits.lua", -- Blox Fruits
+    [136755111277466] = "https://raw.githubusercontent.com/x2zu/loader/refs/heads/main/ObfSource/Anime%20Shadow2.lua", -- Anime Shadow 2
+}
+
+if creator == 2753915549 or supportedGames[game.PlaceId] then
+    local url = supportedGames[game.PlaceId]
+
+    if url == "" then
+        warn("Url doesnt found in this game!:", game.PlaceId)
     else
-        url = "https://raw.githubusercontent.com/x2zu/loader/refs/heads/main/ObfSource/BloxFruits.lua" -- Blox Fruits
+        loadstring(game:HttpGet(url))()
     end
-    loadstring(game:HttpGet(url))()
 else
     warn("Script not supported this game!")
 end
