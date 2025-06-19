6f721065869a87b8cba824b0387d5ff1ba69271a
x2zu <nooelprakasa@gmail.com>
2025-06-19 15:05:10 +0700

    Update scripts.lua

--- scripts.lua
+++ scripts.lua
@@ -17,6 +17,7 @@ local games = {
     [34063840] = 'https://raw.githubusercontent.com/x2zu/loader/refs/heads/main/ObfSource/AnimeRising.lua', -- Anime Rising
     [5096106] = 'https://raw.githubusercontent.com/x2zu/loader/main/ObfSource/AnimeRails.lua', -- Anime Rails
     [35815907] = 'https://raw.githubusercontent.com/x2zu/loader/refs/heads/main/ObfSource/StealABrainrot.lua', -- Steal A Brainrot
+    [12836673] = 'https://raw.githubusercontent.com/x2zu/loader/refs/heads/main/ObfSource/BladeBall.lua', -- Blade Ball
     }
 
 if games[creator] then
