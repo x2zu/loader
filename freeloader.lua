5c3c2b7659920bfb0fe8edfee55e8a4c8401e09f
x2zu <nooelprakasa@gmail.com>
2025-10-27 13:12:25 +0700

    Update freeloader.lua

--- freeloader.lua
+++ freeloader.lua
@@ -1,13 +1,3 @@
-
---[[
-__   __ _____  _______   _ 
-\ \ / // __  \|___  / | | |
- \ V / `' / /'   / /| | | |
- /   \   / /    / / | | | |
-/ /^\ \./ /___./ /__| |_| |
-\/   \/\_____/\_____/\___/
-]]
-
 repeat task.wait() until game.Players.LocalPlayer and game.Players.LocalPlayer.Character
 if not game:IsLoaded() then
     game.Loaded:Wait()
@@ -15,7 +5,7 @@ end
 
 print("Supported game!")
 
-local gameId = game.PlaceId
+local gameId = game.GameId  -- Ganti dari PlaceId ke GameId (UniverseId)
 
 local games = {
     [121864768012064] = 'https://raw.githubusercontent.com/xwwwwwwwwwwwwwwwwwwwqd/loader/main/GamesData/FishItFree.lua', -- Fish It
@@ -24,10 +14,7 @@ local games = {
 
 if games[gameId] then 
     print("Please wait, daddyhh~ loading..")
-    loadstring(game:HttpGet(games[gameId]))()
+    loadstring(game:HttpGet(games[gameId], true))()
 else
     warn("Unsupported game.")
 end
-
-
-
