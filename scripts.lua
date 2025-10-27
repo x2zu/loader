53bbcedef90cb69b41713e5919ec54703e77487e
x2zu <nooelprakasa@gmail.com>
2025-10-27 22:38:02 +0700

    Update scripts.lua

--- scripts.lua
+++ scripts.lua
@@ -19,6 +19,7 @@ local gameId = game.PlaceId
 local games = {
     [121864768012064] = 'https://api.luarmor.net/files/v3/loaders/91c92d3c217d524123cd466ca83c4f16.lua', -- Fish It
     [131716211654599] = 'https://api.luarmor.net/files/v3/loaders/978109b2813eaafe5878888c42527259.lua', -- Fisch
+    [127742093697776] = 'https://api.luarmor.net/files/v3/loaders/a6271160c4b4adca2aa3744e705e07ee.lua', -- Plant Vs Brainrots
 }
 
 if games[gameId] then 
@@ -26,5 +27,6 @@ if games[gameId] then
     loadstring(game:HttpGet(games[gameId]))()
 else
     warn("Unsupported game.")
-end
+end 
+
 
