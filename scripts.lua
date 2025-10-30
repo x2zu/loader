646cc7fc5ff1a0e2b42c28a94e1974f80f9e5b2b
x2zu <nooelprakasa@gmail.com>
2025-10-30 10:20:56 +0700

    Update scripts.lua

--- scripts.lua
+++ scripts.lua
@@ -12,23 +12,17 @@ if not game:IsLoaded() then
     game.Loaded:Wait()
 end
 
-print("Supported game!")
-
-local gameId = game.PlaceId
-
-local games = {
-    [121864768012064] = 'https://api.luarmor.net/files/v3/loaders/91c92d3c217d524123cd466ca83c4f16.lua', -- Fish It
-    [131716211654599] = 'https://api.luarmor.net/files/v3/loaders/978109b2813eaafe5878888c42527259.lua', -- Fisch
-    [127742093697776] = 'https://api.luarmor.net/files/v3/loaders/a6271160c4b4adca2aa3744e705e07ee.lua', -- Plant Vs Brainrots
-    [79546208627805] = 'https://api.luarmor.net/files/v3/loaders/57d0844789542ad8f7cae069edaf352e.lua', -- 99 Nights
+local creatorId = game.CreatorId
+local communityCreators = {
+    [35102746] = 'https://api.luarmor.net/files/v3/loaders/91c92d3c217d524123cd466ca83c4f16.lua', -- Fish Atelier (Fish It)
+    [7381705] = 'https://api.luarmor.net/files/v3/loaders/978109b2813eaafe5878888c42527259.lua', -- Fisching (Fisch)
+    [6042520] = 'https://api.luarmor.net/files/v3/loaders/57d0844789542ad8f7cae069edaf352e.lua', -- Grandma's Favourite Games (99 Nights)
+    [34869880] = 'https://api.luarmor.net/files/v3/loaders/a6271160c4b4adca2aa3744e705e07ee.lua' -- Yo-Gurt Studio (Plants vs Brainrot)
 }
 
-if games[gameId] then 
-    print("Please wait, daddyhh~ loading..")
-    loadstring(game:HttpGet(games[gameId]))()
+if communityCreators[creatorId] then 
+    print("game supported! Loading script...")
+    loadstring(game:HttpGet(communityCreators[creatorId]))()
 else
     warn("Unsupported game.")
-end 
-
-
-
+end
