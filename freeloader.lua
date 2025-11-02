1217c59e6774a4fe2a2de37d1b8858559d796554
x2zu <nooelprakasa@gmail.com>
2025-11-02 11:14:56 +0700

    Update freeloader.lua

--- freeloader.lua
+++ freeloader.lua
@@ -16,8 +16,8 @@ print("Supported game!")
 local creatorId = game.CreatorId
 local communityCreators = {
     [7381705] = 'https://raw.githubusercontent.com/xwwwwwwwwwwwwwwwwwwwqd/loader/main/GamesData/Fisch.lua',
-    [35102746] = 'https://raw.githubusercontent.com/xwwwwwwwwwwwwwwwwwwwqd/loader/refs/heads/main/GamesData/FishItFree.lua', -- Fish It
-    [8818124] = 'https://raw.githubusercontent.com/xwwwwwwwwwwwwwwwwwwwqd/loader/refs/heads/main/GamesData/ViolanceFree.lua', Violance District
+    [35102746] = 'https://raw.githubusercontent.com/xwwwwwwwwwwwwwwwwwwwqd/loader/main/GamesData/FishItFree.lua', -- Fish It
+    [8818124] = 'https://raw.githubusercontent.com/xwwwwwwwwwwwwwwwwwwwqd/loader/main/GamesData/ViolanceFree.lua', Violance District
 }
 
 if communityCreators[creatorId] then 
@@ -26,5 +26,6 @@ if communityCreators[creatorId] then
 else
     warn("Unsupported game.")
 end
+
 
 
