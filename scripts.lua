1fe307019f95f08f25dcb183d0796553813cb54e
x2zu <nooelprakasa@gmail.com>
2025-11-02 11:17:35 +0700

    Update scripts.lua

--- scripts.lua
+++ scripts.lua
@@ -17,8 +17,8 @@ local communityCreators = {
     [35102746] = 'https://api.luarmor.net/files/v3/loaders/91c92d3c217d524123cd466ca83c4f16.lua', -- Fish It
     [7381705] = 'https://api.luarmor.net/files/v3/loaders/978109b2813eaafe5878888c42527259.lua', -- Fisch
     [6042520] = 'https://api.luarmor.net/files/v3/loaders/57d0844789542ad8f7cae069edaf352e.lua', -- 99 Nights
-    [34869880] = 'https://api.luarmor.net/files/v3/loaders/a6271160c4b4adca2aa3744e705e07ee.lua' -- Plants vs Brainrot
-    [8818124] = 'https://api.luarmor.net/files/v3/loaders/49a49c2667090f88ed7589c2c00c9d31.lua' -- Violance District
+    [34869880] = 'https://api.luarmor.net/files/v3/loaders/a6271160c4b4adca2aa3744e705e07ee.lua', -- Plants vs Brainrot
+    [8818124] = 'https://api.luarmor.net/files/v3/loaders/49a49c2667090f88ed7589c2c00c9d31.lua', -- Violance District
 }
 
 if communityCreators[creatorId] then 
@@ -27,6 +27,7 @@ if communityCreators[creatorId] then
 else
     warn("Unsupported game.")
 end
+
 
 
 
