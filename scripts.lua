668a28d9583b04113aad807a2852430fffbd82fe
x2zu <nooelprakasa@gmail.com>
2025-12-27 16:05:15 +0700

    Update scripts.lua

--- scripts.lua
+++ scripts.lua
@@ -15,7 +15,7 @@ print("Supported game!")
 local creatorId = game.CreatorId
 local communityCreators = {
     [35102746] = 'https://api.luarmor.net/files/v3/loaders/91c92d3c217d524123cd466ca83c4f16.lua', -- Fish It
-    [7381705] = 'https://api.luarmor.net/files/v3/loaders/978109b2813eaafe5878888c42527259.lua', -- Fisch
+    [7381705] = 'https://api.luarmor.net/files/v3/loaders/6bc668603e3c93919387e564af72fd88.lua', -- Fisch
     [6042520] = 'https://api.luarmor.net/files/v3/loaders/57d0844789542ad8f7cae069edaf352e.lua', -- 99 Nights
     [34869880] = 'https://api.luarmor.net/files/v3/loaders/a6271160c4b4adca2aa3744e705e07ee.lua', -- Plants vs Brainrot
     [8818124] = 'https://api.luarmor.net/files/v3/loaders/49a49c2667090f88ed7589c2c00c9d31.lua', -- Violance District
@@ -28,6 +28,7 @@ if communityCreators[creatorId] then
 else
     warn("Unsupported game.")
 end
+
 
 
 
