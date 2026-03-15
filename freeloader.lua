c9a7efec0459846aa1aa3e0459a7274ac3ac2eda
x2zu <naelprakasa@gmail.com>
2026-03-15 10:58:42 +0700

    Update freeloader.lua

--- freeloader.lua
+++ freeloader.lua
@@ -1,65 +1,24 @@
---[[ 
-__   __ _____  _______   _ 
-\ \ / // __  \|___  / | | |
- \ V / `' / /'   / /| | | |
- /   \   / /    / / | | | |
-/ /^\ \./ /___./ /__| |_| |
-\/   \/\_____/\_____/\___/
-]]
-
--- local executor = "Unknown"
-
--- pcall(function()
---     if identifyexecutor then
---         executor = identifyexecutor()
---     elseif getexecutorname then
---         executor = getexecutorname()
---     end
--- end)
-
--- executor = string.lower(tostring(executor))
-
--- local blocked = {
---     "xeno",
---     "solara"
--- }
-
--- for _,v in ipairs(blocked) do
---     if string.find(executor, v) then
---         game.Players.LocalPlayer:Kick("Executor not supported. Please use a high UNC executor.")
---         return
---     end
--- end
-
--- local requiredFunctions = {
---     "getgc",
---     "hookfunction",
---     "getgenv"
--- }
-
--- for _,func in ipairs(requiredFunctions) do
---     if not getfenv()[func] then
---         game.Players.LocalPlayer:Kick("Your executor does not support required UNC level.")
---         return
---     end
--- end
-
 repeat task.wait() until game.Players.LocalPlayer and game.Players.LocalPlayer.Character
 if not game:IsLoaded() then
     game.Loaded:Wait()
 end
 print("Supported game!")
+
 local creatorId = game.CreatorId
 local communityCreators = {
-    [7381705] = 'https://raw.githubusercontent.com/x2zu/loader/refs/heads/main/UI/FischFreeNemesis.lua', -- Fisch
-    [35102746] = 'https://raw.githubusercontent.com/x2zu/loader/refs/heads/main/UI/obfuscated_script-1770551344769.lua', -- Fish It
-    [1066523035] = 'https://raw.githubusercontent.com/x2zu/loader/refs/heads/main/UI/TitanFishing.lua' -- Titan Fishing
+    [7381705]    = 'https://raw.githubusercontent.com/x2zu/loader/refs/heads/main/UI/FischFreeNemesis.lua',
+    [35102746]   = 'https://raw.githubusercontent.com/x2zu/loader/refs/heads/main/UI/obfuscated_script-1770551344769.lua',
+    [1066523035] = 'https://raw.githubusercontent.com/x2zu/loader/refs/heads/main/UI/TitanFishing.lua'
 }
 
-if communityCreators[creatorId] then 
+if communityCreators[creatorId] then
     print("game supported! Loading script...")
+
+    if script_key and script_key ~= "" then
+        getgenv().script_key = script_key
+    end
+
     loadstring(game:HttpGet(communityCreators[creatorId]))()
 else
     warn("Unsupported game.")
 end
-
