288b2b8e7b6de21589fa679c218fd809fdb9bf22
x2zu <naelprakasa@gmail.com>
2026-03-19 13:59:43 +0700

    Update freeloader.lua

--- freeloader.lua
+++ freeloader.lua
@@ -20,7 +20,7 @@ local creatorId = game.CreatorId
 local communityCreators = {
         [12836673]   = "https://raw.githubusercontent.com/x2zu/loader/refs/heads/main/UI/BladeBall.lua", 
         [35102746]   = "https://raw.githubusercontent.com/x2zu/loader/refs/heads/main/UI/obfuscated_script-1770551344769.lua", -- Fish It
-        [1002185259] = "https://raw.githubusercontent.com/x2zu/loader/refs/heads/main/UI/SailorPieceFree.lua", -- Sailor Piece
+        [1002185259] = "https://raw.githubusercontent.com/x2zu/loader/refs/heads/main/UI/Sailor%20Piecek.lua", -- Sailor Piece
 }
 
 if communityCreators[creatorId] then 
